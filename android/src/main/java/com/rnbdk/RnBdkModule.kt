e63bafe41ec5a306c67d95d4d111216acddd8c74
Rishabh Kumar <rsbkme@gmail.com>
2022-08-10 00:46:40 +0530

    add bdk to android

--- android/src/main/java/com/rnbdk/RnBdkModule.kt
+++ android/src/main/java/com/rnbdk/RnBdkModule.kt
@@ -1,20 +1,22 @@
 package com.rnbdk
+
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.ReactContextBaseJavaModule
 import com.facebook.react.bridge.ReactMethod
 import com.facebook.react.bridge.Promise
+import org.bitcoindevkit.*
 
-class RnBdkModule(reactContext: ReactApplicationContext) : ReactContextBaseJavaModule(reactContext) {
+class RnBdkModule(reactContext: ReactApplicationContext) :
+  ReactContextBaseJavaModule(reactContext) {
 
-    override fun getName(): String {
-        return "RnBdk"
-    }
+  override fun getName(): String {
+    return "RnBdk"
+  }
 
-    // Example method
-    // See https://reactnative.dev/docs/native-modules-android
-    @ReactMethod
-    fun multiply(a: Int, b: Int, promise: Promise) {
-          promise.resolve(a * b)
-        }
-
-    }
+  // Example method
+  // See https://reactnative.dev/docs/native-modules-android
+  @ReactMethod
+  fun multiply(a: Int, b: Int, promise: Promise) {
+    promise.resolve(a * b)
+  }
+}
--- android/src/main/java/com/rnbdk/RnBdkPackage.kt
+++ android/src/main/java/com/rnbdk/RnBdkPackage.kt
@@ -1,4 +1,5 @@
 package com.rnbdk
+
 import com.facebook.react.ReactPackage
 import com.facebook.react.bridge.NativeModule
 import com.facebook.react.bridge.ReactApplicationContext
@@ -6,11 +7,11 @@ import com.facebook.react.uimanager.ViewManager
 
 
 class RnBdkPackage : ReactPackage {
-    override fun createNativeModules(reactContext: ReactApplicationContext): List<NativeModule> {
-        return listOf(RnBdkModule(reactContext))
-    }
+  override fun createNativeModules(reactContext: ReactApplicationContext): List<NativeModule> {
+    return listOf(RnBdkModule(reactContext))
+  }
 
-    override fun createViewManagers(reactContext: ReactApplicationContext): List<ViewManager<*, *>> {
-        return emptyList()
-    }
+  override fun createViewManagers(reactContext: ReactApplicationContext): List<ViewManager<*, *>> {
+    return emptyList()
+  }
 }
